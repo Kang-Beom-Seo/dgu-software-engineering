b5514aca51128a5d695f520c27e9d1d2046bef5c
Kang-Beom-Seo <79088141+Kang-Beom-Seo@users.noreply.github.com>
2021-06-13 23:42:00 +0900

    Add files via upload

---
+++ DB_handler.py
@@ -0,0 +1,55 @@
+from pymongo import MongoClient
+import json
+import uuid
+
+class DBModule:
+  def __init__(self):
+    client = MongoClient('localhost', 27017)
+    self.db = client.user_db
+
+  def login(self, id, pwd):
+    users = self.db.userInfo.find()
+    for user in users:
+      if id == user["uid"]:
+        if pwd == user["info"]["pwd"]:
+          return True
+    return False
+
+  def signin_verification(self, uid):
+    users = self.db.userInfo.find()
+    for user in users:
+      if uid == user["uid"]:
+        return False
+    return True
+
+  def signin(self, _id_, pwd, name, email):
+    information = {
+      "uid" : _id_,
+      "info" : {
+        "pwd" : pwd,
+        "uname" : name,
+        "email" : email
+      }
+    }
+    self.signin_verification(_id_)
+    self.db.userInfo.save(information)
+
+  def write_post(self, title, contents, uid):
+    pid = str(uuid.uuid4())[:10]
+    information = {
+      "title" : title,
+      "contents" : contents,
+      "uid" : uid,
+      "pid" : pid
+    }
+    self.db.post.save(information)
+
+  def post_list(self, uid):
+    post_lists = self.db.post.find({"uid" : uid})
+    return post_lists
+  def post_detail(self, uid, pid):
+    post = self.db.post.find({"uid" : uid, "pid" : pid})
+    return post
+
+  def get_user(self, uid):
+    pass
---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask, redirect, render_template, url_for, request, flash, session
+from DB_handler import DBModule
+
+app = Flask(__name__)
+app.secret_key = "My-diary"
+DB = DBModule()
+
+@app.route('/') # 홈페이지
+def index():
+  if "uid" in session:
+    user = session["uid"]
+  else:
+    user = "Login"
+  return render_template("index.html", user=user)
+
+@app.route("/list")
+def post_list():
+  post_list = DB.post_list(session["uid"])
+  return render_template("post_list.html", post_list=post_list)
+
+@app.route("/post/<string:pid>")
+def post(pid):
+  posts = DB.post_detail(session["uid"], pid)
+  return render_template("post_detail.html", post=posts)
+
+@app.route("/login")
+def login():
+  if "uid" in session:
+      return redirect(url_for("index"))
+  return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+  if "uid" in session:
+    session.pop("uid")
+    return redirect(url_for("index"))
+  else:
+   return redirect(url_for("login"))
+
+@app.route("/login_done", methods = ["get"])
+def login_done():
+  uid = request.args.get("id")
+  pwd = request.args.get("pwd")
+  if DB.login(uid, pwd):
+    session["uid"] = uid
+    return redirect(url_for("index"))
+  else:
+    flash("아이디나 비밀번호가 틀립니다.")
+    return redirect(url_for("login"))
+
+@app.route("/signin")
+def signin():
+  return render_template("signin.html")
+
+@app.route("/signin_done")
+def signin_done():
+  email = request.args.get("email")
+  uid = request.args.get("id")
+  pwd = request.args.get("pwd")
+  name = request.args.get("name")
+  if DB.signin(email=email, _id_=uid, pwd=pwd, name=name):
+    return redirect(url_for("index"))
+  else:
+    flash("이미 가입된 아이디 입니다.")
+    return redirect(url_for("signin"))
+
+@app.route("/user/<uid>")
+def user(uid):
+  pass
+
+@app.route("/write")
+def write():
+  if "uid" in session:
+    return render_template("write_post.html")
+  else:
+    return redirect(url_for("login"))
+
+@app.route("/write_done", methods = ["GET"])
+def write_done():
+  title = request.args.get("title")
+  contents = request.args.get("contents")
+  uid = session.get("uid")
+  DB.write_post(title, contents, uid)
+  return redirect(url_for("index"))
+
+if __name__ == "__main__":
+  app.run(debug=True)
